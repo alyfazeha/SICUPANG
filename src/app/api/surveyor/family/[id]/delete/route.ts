9cf73875bd41eb362ed1fef100da84b2a10095e1
a6iyyu <mizukinako7@gmail.com>
2025-10-10 07:29:28 +0700

    :beers: Fix delete family data in surveyor page.

--- src/app/api/surveyor/family/[id]/delete/route.ts
+++ src/app/api/surveyor/family/[id]/delete/route.ts
@@ -1,29 +1,21 @@
-import { unlink } from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
-import { join } from "path";
-import { cwd } from "process";
+import { del } from "@vercel/blob";
 import { Prisma } from "@/lib/prisma";
 
-export async function DELETE(_request: NextRequest, context: { params: Promise<{ id: string }> }): Promise<NextResponse> {
+export async function DELETE(_request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   try {
-    const { id } = await context.params;
+    const { id } = await params;
 
-    const documentation = await Prisma.keluarga.findUnique({
+    const familyToDelete = await Prisma.keluarga.findUnique({
       where: { id_keluarga: parseInt(id, 10) },
       select: { gambar: true },
     });
 
-    if (documentation?.gambar) {
-      const path = join(cwd(), "public", documentation.gambar);
-      await unlink(path);
-    }
-
-    await Prisma.pangan_keluarga.deleteMany({ where: { id_keluarga: parseInt(id, 10) } });
+    if (familyToDelete?.gambar) await del(familyToDelete.gambar);
     await Prisma.keluarga.delete({ where: { id_keluarga: parseInt(id, 10) } });
-
-    return NextResponse.json({ message: "✅ Data keluarga berhasil dihapus" }, { status: 200 });
-  } catch (err: unknown) {
-    console.error(`Gagal menghapus data keluarga: ${err}`);
-    return NextResponse.json({ message: "❌ Gagal menghapus data keluarga" }, { status: 500 });
+    return NextResponse.json({ message: "Data keluarga berhasil dihapus." });
+  } catch (error) {
+    console.error(`❌ Error DELETE /api/surveyor/family/[id]/delete: ${error}`);
+    return NextResponse.json({ error: "Gagal menghapus data keluarga." }, { status: 500 });
   }
 }
